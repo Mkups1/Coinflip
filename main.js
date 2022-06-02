cd356929196949461201847ff68419825078f714
Mkups1 <m.kupilik1@share.epsb.ca>
2022-06-02 13:24:43 -0600

    Coin flip done

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 // Coin flip 
+let resultEl = document.getElementById("result-span")
+let coinResultEl = document.getElementById("coin-result")
+
+
 
 document.getElementById("button").addEventListener("click", flip)
 
 function flip(){
-
+    let flip = Math.random()
+    console.log(flip)
+    if (flip > 0.5){
+        resultEl.innerHTML = "Tails"
+        coinResultEl.src = "img/Coin-tails.png"
+    } else if(flip < 0.5){
+        resultEl.innerHTML = "Heads"
+        coinResultEl.src = "img/Coin-heads.png"
+    } else{
+        resultEl.innerHtml = "side"
+    }
 }
