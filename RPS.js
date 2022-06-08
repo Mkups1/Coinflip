67279c06187c2949531c4624286924febc42a10b
Mkups1 <m.kupilik1@share.epsb.ca>
2022-06-08 13:24:53 -0600

    Absolutely nothing

---
+++ RPS.js
@@ -0,0 +1,12 @@
+// Rock, Paper, Scissors
+
+let rock = 
+
+document.getElementById("rock-image").addEventListener("click", rockAns)
+document.getElementById("paper.png").addEventListener("click", paperAns)
+document.getElementById("scissors.png").addEventListener("click", scissorsAns)
+
+function rockAns(){
+    console.log("hi")
+
+}
