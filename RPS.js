caa8e986657976b37ea85912c0c839678101481d
Mkups1 <m.kupilik1@share.epsb.ca>
2022-06-09 13:25:02 -0600

    RPS working

--- RPS.js
+++ RPS.js
@@ -1,12 +1,78 @@
 // Rock, Paper, Scissors
 
-let rock = 
+let rockEl = document.getElementById("rock-image")
+let paperEl = document.getElementById("paper-image")
+let scissorsEl = document.getElementById("scissors-image")
+let checkmarkEl = document.getElementById("check")
+let pScoreEl = document.getElementById("p-score")
+let pScore = 0
+let oScoreEl = document.getElementById("o-score")
+let oScore = 0
+
 
 document.getElementById("rock-image").addEventListener("click", rockAns)
-document.getElementById("paper.png").addEventListener("click", paperAns)
-document.getElementById("scissors.png").addEventListener("click", scissorsAns)
+document.getElementById("paper-image").addEventListener("click", paperAns)
+document.getElementById("scissors-image").addEventListener("click", scissorsAns)
 
 function rockAns(){
-    console.log("hi")
+    let opAns = 1
+    opAns = Math.random() * opAns
+    console.log(opAns)
+    paperEl.style.display="none"
+    scissorsEl.style.display="none"
+    if(opAns <= 0.33){
+        checkmarkEl.src = "img/scissors.png"
+        pScore++
+        pScoreEl.innerHTML = pScore
+    } else if(opAns <= 0.66){
+        checkmarkEl.src = "img/paper.png"
+        oScore++
+        oScoreEl.innerHTML = oScore
+    } else{
+        checkmarkEl.src = "img/rock.png"
+    }
+
+
+}
+
+function paperAns(){
+    let opAns = 1
+    opAns = Math.random() * opAns
+    console.log(opAns)
+    rockEl.style.display="none"
+    scissorsEl.style.display="none"
+    if(opAns <= 0.33){
+        checkmarkEl.src = "img/scissors.png"
+        oScore++
+        oScoreEl.innerHTML = oScore
+    } else if(opAns <= 0.66){
+        checkmarkEl.src = "img/paper.png"
+    } else{
+        checkmarkEl.src = "img/rock.png"
+        pScore++
+        pScoreEl.innerHTML = pScore
+    }
+
+
+}
+
+function scissorsAns(){
+    let opAns = 1
+    opAns = Math.random() * opAns
+    console.log(opAns)
+    paperEl.style.display="none"
+    rockEl.style.display="none"
+    if(opAns <= 0.33){
+        checkmarkEl.src = "img/scissors.png"
+    } else if(opAns <= 0.66){
+        checkmarkEl.src = "img/paper.png"
+        pScore++
+        pScoreEl.innerHTML = pScore
+    } else{
+        checkmarkEl.src = "img/rock.png"
+        oScore++
+        oScoreEl.innerHTML = oScore
+    }
+
 
 }
