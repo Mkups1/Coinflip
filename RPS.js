ade9e6d9173253d17e01f1e3b70cd9c25fe8947e
Mkups1 <m.kupilik1@share.epsb.ca>
2022-06-10 13:23:51 -0600

    Worked on making it better

--- RPS.js
+++ RPS.js
@@ -8,6 +8,16 @@ let pScoreEl = document.getElementById("p-score")
 let pScore = 0
 let oScoreEl = document.getElementById("o-score")
 let oScore = 0
+let messageEl = document.getElementById("message")
+let start = true
+let restartTime = setTimeout(startF, 3000)
+
+function startF(){
+    paperEl.style.display="block"
+    scissorsEl.style.display="block"
+    start = true
+    clearTimeout(restartTime)
+}
 
 
 document.getElementById("rock-image").addEventListener("click", rockAns)
@@ -15,23 +25,32 @@ document.getElementById("paper-image").addEventListener("click", paperAns)
 document.getElementById("scissors-image").addEventListener("click", scissorsAns)
 
 function rockAns(){
-    let opAns = 1
-    opAns = Math.random() * opAns
-    console.log(opAns)
-    paperEl.style.display="none"
-    scissorsEl.style.display="none"
-    if(opAns <= 0.33){
-        checkmarkEl.src = "img/scissors.png"
-        pScore++
-        pScoreEl.innerHTML = pScore
-    } else if(opAns <= 0.66){
-        checkmarkEl.src = "img/paper.png"
-        oScore++
-        oScoreEl.innerHTML = oScore
-    } else{
-        checkmarkEl.src = "img/rock.png"
-    }
+    if(start === true){
+        let opAns = 1
+        opAns = Math.random() * opAns
+        console.log(opAns)
+        paperEl.style.display="none"
+        scissorsEl.style.display="none"
+        start = false
+        if(opAns <= 0.33){
+            checkmarkEl.src = "img/scissors.png"
+            pScore++
+            pScoreEl.innerHTML = pScore
+            messageEl.innerHTML = "You Win!"
+        } else if(opAns <= 0.66){
+            checkmarkEl.src = "img/paper.png"
+            oScore++
+            oScoreEl.innerHTML = oScore
+         messageEl.innerHTML = "You Lose!"
+        } else{
+            checkmarkEl.src = "img/rock.png"
+            messageEl.innerHTML = "Tie!"
+        }
+        if(start === false){
+            restartTime
+        }
 
+    }    
 
 }
 
@@ -45,12 +64,15 @@ function paperAns(){
         checkmarkEl.src = "img/scissors.png"
         oScore++
         oScoreEl.innerHTML = oScore
+        messageEl.innerHTML = "You lose!"
     } else if(opAns <= 0.66){
         checkmarkEl.src = "img/paper.png"
+        messageEl.innerHTML = "Tie!"
     } else{
         checkmarkEl.src = "img/rock.png"
         pScore++
         pScoreEl.innerHTML = pScore
+        messageEl.innerHTML = "You Win!"
     }
 
 
@@ -64,14 +86,17 @@ function scissorsAns(){
     rockEl.style.display="none"
     if(opAns <= 0.33){
         checkmarkEl.src = "img/scissors.png"
+        messageEl.innerHTML = "Tie!"
     } else if(opAns <= 0.66){
         checkmarkEl.src = "img/paper.png"
         pScore++
         pScoreEl.innerHTML = pScore
+        messageEl.innerHTML = "You Win!"
     } else{
         checkmarkEl.src = "img/rock.png"
         oScore++
         oScoreEl.innerHTML = oScore
+        messageEl.innerHTML = "You Lose!"
     }
 
 
